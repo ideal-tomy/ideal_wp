9b3db1f09f4867de6c61c5a6f9168a0f8588bef6
ideal-tomy <ryojitomii@gmail.com>
2025-10-17 01:58:35 +0900

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter', 'Noto Sans JP', 'sans-serif'],
+            },
+            colors: {
+                'background-dark': '#18181b', // zinc-900
+                'accent-gold': '#fbbf24', // amber-400
+                'section-bg': '#27272a', // zinc-800
+            }
+        }
+    }
+}
