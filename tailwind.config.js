5311a2f0affc3148dfb7bb67076a380015a23b98
ideal-tomy <ryojitomii@gmail.com>
2025-10-18 22:07:28 +0900

    基礎完成06

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,17 @@ tailwind.config = {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Inter', 'Noto Sans JP', 'sans-serif'],
+                // 変更後の設定
+                sans: [
+                    'Yu Mincho',     // 游明朝 (Mac/Windowsの一部環境)
+                    'Hiragino Mincho ProN', // ヒラギノ明朝 (Mac)
+                    'MS PMincho',    // MS PMincho (Windows)
+                    'serif',         // 最後のフォールバックとして「serif (明朝体)」を指定
+                ],
             },
             colors: {
                 'background-dark': '#18181b', // zinc-900
-                'accent-gold': '#fbbf24', // amber-400
+                'accent-gold': '#8f14cc', // amber-400
                 'section-bg': '#27272a', // zinc-800
             }
         }
